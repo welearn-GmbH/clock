e81cfa11e4d16b366b515b9f92c49ec726dd4f1a
Michael Spahn <michael.spahn@wehorse.com>
2022-10-24 12:28:01 +0200

    Implement PSR-20 draft for now, we maintain our own interface to avoid surprises

--- src/ClockInterface.php
+++ src/ClockInterface.php
@@ -7,7 +7,7 @@ use DateTimeImmutable;
 /**
  * @see https://github.com/php-fig/fig-standards/blob/master/proposed/clock.md
  */
-interface ClockInterface
+interface ClockInterface extends \Psr\Clock\ClockInterface
 {
     public function now(): DateTimeImmutable;
 }
